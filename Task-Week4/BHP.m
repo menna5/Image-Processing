93b4cb54c1d91efc83b8442e271c9b8e5ed60830
Menna Omran <mena08800@gmail.com>
2022-12-11 00:11:17 +0200

    Add files via upload

---
+++ Task-Week4/BHP.m
@@ -0,0 +1,19 @@
+function [newImg] = BHP(img, d0, n)
+[H, W] = size(img);
+mask = zeros(H, W);
+for h=1:H
+    for w=1:W
+        d = sqrt((W/2-w)^2+(H/2-h)^2);
+        mask(h, w) = 1/(1+(d0/d)^(2*n));
+    end
+end
+
+IMG_F = fft2(img);
+IMG_shifted = fftshift(IMG_F);
+R = real(IMG_shifted).*mask;
+Im = imag(IMG_shifted).*mask;
+FIMG = R + i * Im;
+inversed = ifft2(FIMG);
+newImg = uint8(inversed);
+end
+
