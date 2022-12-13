765f0bce8f9792670153c811c99cebaf17fec6ac
Menna Omran <mena08800@gmail.com>
2022-12-13 14:45:01 +0200

    Add files via upload

---
+++ Task-Week5/SaltandPepper.m
@@ -0,0 +1,17 @@
+function [newImg] = SaltandPepper(img, ps, pp)
+[W, H] = size(img);
+newImg = img;
+numS = ps * (H * W);
+numP = pp * (H * W);
+for i=1:93
+    x = round(rand(1, 1)*H);
+    y = round(rand(1, 1)*W);
+    newImg(x, y) = 0;
+end
+for i=1:91
+    x = round(rand(1, 1)*H);
+    y = round(rand(1, 1)*W);
+    newImg(x, y) = 255;
+end
+end
+
